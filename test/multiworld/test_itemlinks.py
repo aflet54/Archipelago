552ec909b8606779d340c0c94f64c9dbdbffcf59
aflet54 <aflet54@gmail.com>
2024-04-11 10:21:57 +1000

    Figuring out basic test cases

--- test/multiworld/test_itemlinks.py
+++ test/multiworld/test_itemlinks.py
@@ -1,17 +1,22 @@
-import unittest
+
 from unittest import TestCase
 
 from BaseClasses import MultiWorld
-from test.general import setup_multiworld
+from worlds.AutoWorld import AutoWorldRegister
+from ..general import setup_multiworld
 
 class ItemLinkTestBase(TestCase):
     multiworld: MultiWorld
 
 
-class TestItemLinkDefaults(ItemLinkTestBase):
+class TestTwoPlayerItemLink(ItemLinkTestBase):
     def test_defaults(self) -> None:
         """Tests that the ItemLink Defaults are set correctly."""
         # Prepare
+        for world in AutoWorldRegister.world_types.values():
+            self.multiworld = setup_multiworld([world, world], ())
+            
+
 
         # Act
 
