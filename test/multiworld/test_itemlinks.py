340d0beca438afb3b67259f0882d53c17f2afe04
aflet54 <aflet54@gmail.com>
2024-04-11 10:51:07 +1000

    Moved the ItemLink Functions into the Multiworld class

--- test/multiworld/test_itemlinks.py
+++ test/multiworld/test_itemlinks.py
@@ -3,7 +3,8 @@ from unittest import TestCase
 
 from BaseClasses import MultiWorld
 from worlds.AutoWorld import AutoWorldRegister
-from ..general import setup_multiworld
+from ..general import gen_steps, setup_multiworld
+
 
 class ItemLinkTestBase(TestCase):
     multiworld: MultiWorld
@@ -14,10 +15,12 @@ class TestTwoPlayerItemLink(ItemLinkTestBase):
         """Tests that the ItemLink Defaults are set correctly."""
         # Prepare
         for world in AutoWorldRegister.world_types.values():
+            if not ("Progression Items" in world.item_name_groups.keys()):
+                continue    # We only run this test on worlds that have a "Progression Items" group
+
             self.multiworld = setup_multiworld([world, world], ())
             
-
-
+            self.multiworld.calculate_item_links()
         # Act
 
         # Assert
