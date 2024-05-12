5d6166f15a975a1c5d8430bc1a9823fb12bbeb67
aflet54 <aflet54@gmail.com>
2024-05-13 05:59:33 +1000

    updating tests

--- test/multiworld/test_itemlinks.py
+++ test/multiworld/test_itemlinks.py
@@ -32,17 +32,34 @@ class TestTwoPlayerItemLink(ItemLinkTestBase):
         """Tests that all worlds are able to link items to each other in a multiworld."""
         # Prepare
         for world in AutoWorldRegister.world_types.values():
-            self.multiworld = setup_multiworld([world, world], ())
-            linked_items = self.get_partial_world_items(world)
-            for world in self.multiworld.worlds.values():
-                world.options.accessibility.value = Accessibility.option_locations
-                world.options.item_links = ItemLinks([{'name': 'ItemLinkTest', 'item_pool' : linked_items, 'replacement_item' : None, 'link_replacement' : None}])
-        # Act
-            self.assertSteps(pre_link_steps)
-            self.multiworld.set_item_links()
-            self.multiworld.calculate_item_links()
-        # Assert
-            self.assertTrue(any(group['name'] == "ItemLinkTest" for group in self.multiworld.groups.values()), f"ItemLinkTest group not found in {self.multiworld.groups}")
-            link_group = [group for group in self.multiworld.groups.values() if group['name'] == "ItemLinkTest"][0]
-            self.assertEqual(link_group['game'], world.game, f"Game is not set correctly for {world.game}")
-            self.assertEqual(link_group['item_pool'], set(linked_items), f"Item pool is not set correctly for {world.game}")           
+            with self.subTest(game=world.game):
+                self.multiworld = setup_multiworld([world, world], pre_link_steps)
+                linked_items = self.get_partial_world_items(world)
+                
+                for world in self.multiworld.worlds.values():
+                    world.options.accessibility.value = Accessibility.option_locations
+                    world.options.item_links = ItemLinks([{'name': 'ItemLinkTest', 'item_pool' : linked_items, 'replacement_item' : None, 'link_replacement' : None}])
+                
+                world_items = {}
+                for player in range(1, self.multiworld.players+1):
+                    world_items[player] = [item for item in self.multiworld.itempool if item.player == player]
+
+                # Act
+                prior_item_count = len(self.multiworld.itempool)
+                self.multiworld.set_item_links()
+                self.multiworld.calculate_item_links()
+                # Assert Link was created and contains items
+                self.assertTrue(any(group['name'] == "ItemLinkTest" for group in self.multiworld.groups.values()), f"ItemLinkTest group not found in {self.multiworld.groups}")
+                link_group = [group for group in self.multiworld.groups.values() if group['name'] == "ItemLinkTest"][0]
+                self.assertEqual(link_group['game'], world.game, f"Game is not set correctly for {world.game}")
+                self.assertEqual(link_group['item_pool'], set(linked_items), f"Item pool is not set correctly for {world.game}")
+                self.assertEqual(len(self.multiworld.itempool), prior_item_count, f"ItemPool length should stay the same")
+                for player in range(1, self.multiworld.players+1):
+                    new_world_items = [item for item in self.multiworld.itempool if item.player == player]
+                    self.assertLessEqual(len(new_world_items), len(world_items[player]), f"Item count for each player should be the same or less. We have {len(linked_items)} linked items")
+                # Assert Items are linked
+                for world in self.multiworld.worlds.values():
+                    for item in linked_items:
+                        self.assertEqual(world.item_links[item], link_group['replacement_item'], f"Item {item} is not linked correctly in {world.game}")
+                        self.assertEqual(world.link_replacements[link_group['replacement_item']], link_group['link_replacement'], f"Link replacement is not set correctly in {world.game}")
+            
